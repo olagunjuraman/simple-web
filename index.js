c2a2f2e2f5d023cb6d40515e3d45d7c7f48e5c92
raman <olagunjuraman@gmail.com>
2020-11-20 06:23:36 +0100

    added script

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hi there motherfucker, you must be very stupid");
+  res.send(
+    "hi there motherfucker, you must be very stupid, please make sure to work"
+  );
 });
 
 app.listen(8080, () => {
